bd350381e1e7294c150e33927337f691137bdcfc
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-11-25 10:36:45 +0530

    initial commit

---
+++ src/main/java/com/barath/app/SpringMustacheTemplateExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.barath.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMustacheTemplateExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMustacheTemplateExamplesApplication.class, args);
+	}
+}
---
+++ src/main/java/com/barath/app/TemplateController.java
@@ -0,0 +1,8 @@
+package com.barath.app;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TemplateController {
+
+}
